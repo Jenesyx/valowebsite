cdafc04ba1eceedd2d5d9ce5e95927f86923632a
Jenesyx <a.bidkhori2004@gmail.com>
2024-02-13 12:35:55 +0100

    0.2.0 - Finished the Header, added SlideShow

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,35 @@
 import React from 'react'
 import './Header.css'
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import Home from '../../Images/Home.png'
 import Valorant from '../../Images/Valorant.png'
 import AboutUs from '../../Images/About-us.png'
 import Mentoring from '../../Images/Mentoring.png'
 import Terms from '../../Images/Terms.png'
 import Signin from '../../Images/Signin.png'
+import discord from '../../Images/discord.png'
+import tiktok from '../../Images/tiktok.png'
+import Image1 from '../../Images/Image1.png'
+import Image2 from '../../Images/Image2.png'
+import Image4 from '../../Images/Image4.png'
+import Image5 from '../../Images/Image5.png'
+import Image6 from '../../Images/Image6.png'
+import Image7 from '../../Images/Image7.png'
 
 function Header() {
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 10000,
+    };
+
     return (
         <>
             <nav>
@@ -27,17 +49,43 @@ function Header() {
                         </ul>
                     </div>
                 </header>
+                <div className="slider">
+                <Slider {...settings}>
+                    <div>
+                        <img src={Image1} alt="Slide 1" />
+                    </div>
+                    <div>
+                        <img src={Image2} alt="Slide 2" />
+                    </div>
+                    <div>
+                        <img src={Image4} alt="Slide 4" />
+                    </div>
+                    <div>
+                        <img src={Image5} alt="Slide 5" />
+                    </div>
+                    <div>
+                        <img src={Image6} alt="Slide 6" />
+                    </div>
+                    <div>
+                        <img src={Image7} alt="Slide 7" />
+                    </div>
+                </Slider>
+                </div>
                 <div className="nav">
                     <div className="nav-left">
-                        <p>it's <span>Time</span><br/>to change!</p>
+                        <p>It's <span>Time</span><br/>to change!</p>
                     </div>
                     <div className="nav-right">
                         <div className="discord">
-                            <img src="" alt="" />
+                            <div className="nav-image">
+                                <img src={discord} alt="discord-icon" />
+                            </div>
                             <p>Discord</p>
                         </div>
-                        <div className="Tiktok">
-                            <img src="" alt="" />
+                        <div className="tiktok">
+                            <div className="nav-image">
+                                <img src={tiktok} alt="tiktok-icon" />
+                            </div>
                             <p>TikTok</p>
                         </div>
                     </div>
