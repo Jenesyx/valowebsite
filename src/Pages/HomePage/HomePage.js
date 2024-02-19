658ade1d5a27786d6cb0ce5783ec49bbe064551f
Jenesyx <a.bidkhori2004@gmail.com>
2024-02-19 13:19:44 +0100

    0.3.0 - Added Valorant services section, header updated

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -6,6 +6,19 @@ function HomePage() {
     return (
         <>
             <Header/>
+            <section className="valo-services">
+                <div className="left-services">
+                    <p><span>Valorant</span> Section</p>
+                </div>
+                <div className="right-services">
+                    <div className="boosting both-services">
+                        <p><span>Valorant</span><br />Boosting</p>
+                    </div>
+                    <div className="accounts both-services">
+                        <p><span>Valorant</span><br />Accounts</p>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
