6159acb1402d6f4f8256a518c8f1a9aa01680e90
Jenesyx <a.bidkhori2004@gmail.com>
2024-06-15 16:06:35 +0200

    0.7.8 - Packages updated, added boosting in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './Pages/HomePage/HomePage';
+import Boosting from './Pages/Boosting/Boosting';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<HomePage/>}/>
+          <Route path="/Boosting" element={<Boosting/>}/>
         </Routes>
       </Router>
     </div>
